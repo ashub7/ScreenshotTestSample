f8bcb1716111aa6bdd3693234743f27c82995034
Adm <ashubhandari77@gmail.com>
2024-06-17 13:17:34 +0530

    login button SS test added

--- app/src/main/java/com/manta/composescreenshot/ui/login/LoginScreen.kt
+++ app/src/main/java/com/manta/composescreenshot/ui/login/LoginScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Android
-import androidx.compose.material.icons.outlined.Key
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -27,46 +26,23 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.manta.composescreenshot.ui.login.components.PasswordField
-import com.manta.composescreenshot.ui.login.components.UserIdField
 import com.manta.composescreenshot.ui.theme.ComposescreenshotTheme
 
 @Composable
 fun LoginScreen() {
     Surface {
-        var uiState by remember { mutableStateOf(LoginUiState()) }
         Column(
+            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .fillMaxSize()
-                .padding(horizontal = 30.dp)
+                .padding(horizontal = 20.dp)
         ) {
-
-            Spacer(modifier = Modifier.height(100.dp))
-            Icon(
-                Icons.Outlined.Android,
-                modifier = Modifier.size(100.dp),
-                contentDescription = "",
-                tint = MaterialTheme.colorScheme.primary
-            )
-            Spacer(modifier = Modifier.height(50.dp))
-            UserIdField(
-                value = uiState.userId,
-                onChange = { data -> uiState = uiState.copy(userId = data) },
-                modifier = Modifier.fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.height(20.dp))
-            PasswordField(
-                value = uiState.passsword,
-                onChange = { data -> uiState = uiState.copy(passsword = data) },
-                submit = {},
-                modifier = Modifier.fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.height(40.dp))
             Button(
                 onClick = {},
-                shape = RoundedCornerShape(10.dp),
-                modifier = Modifier.fillMaxWidth()
+                shape = RoundedCornerShape(25.dp),
+                modifier = Modifier.fillMaxWidth().
+                padding(horizontal = 20.dp)
             ) {
                 Text("Login")
             }
